d684a18342f04a6f43989f6a3b18de5cfece34f8
mohammad-khalilpour <kian.khalilpour@gmail.com>
2023-11-10 20:54:46 +0330

    created Chatbot model

--- chatbots/models.py
+++ chatbots/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+import os
 
-# Create your models here.
+
+def chatbot_image_path(instance, filename):
+    ext = filename.split('.')[-1]
+    unique_filename = f"chatbot_{instance.user.id}.{ext}"
+
+    return os.path.join('chatbot_images/', unique_filename)
+
+
+class Chatbot(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=500, blank=True)
+    custom_prompt = models.TextField(blank=True)
+    chatbot_picture = models.ImageField(blank=True, upload_to=chatbot_image_path)
+
