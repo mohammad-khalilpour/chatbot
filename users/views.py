18ae04f339e23d76e4dc296ee25ba5df54c7d059
mohammad-khalilpour <kian.khalilpour@gmail.com>
2023-11-11 16:52:52 +0330

    created register view and configured the url for the view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from users.forms import RegistrationForm
 
-# Create your views here.
+
+def register(request):
+    register_form = RegistrationForm()
+
+    if request.method == "POST":
+        register_form = RegistrationForm(request.POST)
+
+        if register_form.is_valid():
+            user = register_form.save()
+            user.set_password(user.password)
+            user.save()
+
+    return render(request, 'register.html', {'form': register_form})
