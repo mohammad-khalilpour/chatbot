66699119296e9eb191258184845ee5a37890b595
mohammad-khalilpour <kian.khalilpour@gmail.com>
2023-11-11 22:44:28 +0330

    added LoginForm and user_login view and configured the url

--- users/forms.py
+++ users/forms.py
@@ -1,14 +1,16 @@
 from django import forms
 from django.contrib.auth.forms import BaseUserCreationForm
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 User = get_user_model()
 
 
 class RegistrationForm(BaseUserCreationForm):
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': 'email'}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'password1'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'password2'}))
+    email = forms.EmailField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
 
     class Meta:
         model = User
@@ -19,3 +21,60 @@ class RegistrationForm(BaseUserCreationForm):
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError("This email address is already in use.")
         return email
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField()
+    password = forms.CharField()
+
+    error_messages = {
+        "invalid_login": _(
+            "Please enter a correct %(username)s and password. Note that both "
+            "fields may be case-sensitive."
+        ),
+        "inactive": _("This account is inactive."),
+    }
+
+    def __init__(self, request=None, *args, **kwargs):
+
+        self.request = request
+        self.user_cache = None
+        super().__init__(*args, **kwargs)
+
+        self.username_field = User._meta.get_field(User.USERNAME_FIELD)
+
+    def clean(self):
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        print(email)
+        print(password)
+
+        if email is not None and password:
+            self.user_cache = authenticate(
+                self.request, email=email, password=password
+            )
+            if self.user_cache is None:
+                raise self.get_invalid_login_error()
+            else:
+                self.confirm_login_allowed(self.user_cache)
+
+        return self.cleaned_data
+
+    def confirm_login_allowed(self, user):
+        if not user.is_active:
+            raise ValidationError(
+                self.error_messages["inactive"],
+                code="inactive",
+            )
+
+    def get_user(self):
+        return self.user_cache
+
+    def get_invalid_login_error(self):
+        return ValidationError(
+            self.error_messages["invalid_login"],
+            code="invalid_login",
+            params={"email": self.username_field.verbose_name},
+        )
+
+
--- users/views.py
+++ users/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
-from users.forms import RegistrationForm
+from users.forms import RegistrationForm, LoginForm
+from django.contrib.auth import authenticate, login, get_user_model
+
+User = get_user_model()
 
 
 def register(request):
@@ -9,8 +12,19 @@ def register(request):
         register_form = RegistrationForm(request.POST)
 
         if register_form.is_valid():
-            user = register_form.save()
+            user = User(register_form.save())
             user.set_password(user.password)
-            user.save()
+            user.create()
 
     return render(request, 'register.html', {'form': register_form})
+
+
+def user_login(request):
+    if request.method == "POST":
+        login_form = LoginForm(request, data=request.POST)
+
+        if login_form.is_valid():
+            return redirect('https://torob.com/')
+
+    return render(request, 'login.html')
+
