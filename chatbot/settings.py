da39b13d0b8346dadc4af8adb9b02a5b2a7db5f4
mohammad-khalilpour <kian.khalilpour@gmail.com>
2023-11-18 09:27:53 +0330

    added csrf trusted origins

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -38,7 +38,13 @@ ALLOWED_HOSTS.extend(
     )
 )
 
-
+CSRF_TRUSTED_ORIGINS = []
+CSRF_TRUSTED_ORIGINS.extend(
+    filter(
+        None,
+        os.environ.get('CSRF_TRUSTED_ORIGINS', '').split(','),
+    )
+)
 
 # Application definition
 
