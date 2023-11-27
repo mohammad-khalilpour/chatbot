8d2e765404c0f2c9f432afbe4a864842f32a0cc5
mohammad-khalilpour <kian.khalilpour@gmail.com>
2023-11-27 19:35:54 +0330

    saved messages_embedding in test_rag_efficiency

--- chatbots/management/commands/test_rag_efficiency.py
+++ chatbots/management/commands/test_rag_efficiency.py
@@ -1,18 +1,19 @@
 import json
+import os
 from django.core.management.base import BaseCommand
 from chatbots.models import Chatbot, Content
 from conversations.models import Message, Conversation
 from users.models import User
-from chatbots.admin import ContentAdmin
-from django.contrib import admin
 from django.http import HttpRequest
 from conversations.utils import most_related_document
+from conversations.utils import get_embedding
 
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
         json_file_path = 'chatbots/management/data/data.jsonl'
+        questions_embedding_file_path = 'chatbots/management/data/questions_embedding.jsonl'
 
         user = User.objects.filter(email='RagTest@gmail.com').first()
         if not user:
@@ -24,34 +25,46 @@ class Command(BaseCommand):
 
         conversation = Conversation.objects.filter(chatbot=chatbot, user=user).first()
         if not conversation:
-            conversation = Conversation.objects.create(user=user,chatbot=chatbot)
+            conversation = Conversation.objects.create(user=user, chatbot=chatbot)
 
         count = 0
         correct_count = 0
         with open(json_file_path, 'r', encoding='utf-8') as file:
-
             for i, line in enumerate(file):
                 entry = json.loads(line)
                 content = Content.objects.filter(content=entry['doc'], chatbot=chatbot).first()
                 if not content:
-                    content = Content(
-                        chatbot=chatbot,
-                        content=entry['doc']
-                    )
-                    request = HttpRequest()
-                    request.method = 'POST'
-                    request.POST = {'content': entry['doc']}
-                    ContentAdmin(Content, admin.site).save_model(request, content, None, False)
+                    Content.objects.create(chatbot=chatbot, content=entry['doc'], embedding=get_embedding(entry[doc]))
                 self.stdout.write(self.style.SUCCESS(f'doc number {i} added successfully'))
 
             file.seek(0)
 
+            questions = {}
+            if os.path.isfile(questions_embedding_file_path):
+                with open(questions_embedding_file_path, 'r', encoding='utf-8') as question_embedding_file:
+                    for i, line in enumerate(question_embedding_file):
+                        entry = json.loads(line)
+                        questions[entry['question']] = entry['embedding']
+
+            with open(questions_embedding_file_path, 'a', encoding='utf-8') as question_embedding_file:
+                for i, line in enumerate(file):
+                    entry = json.loads(line)
+                    if entry['question'] not in questions:
+                        questions_embedding = get_embedding(entry['question'])
+                        question_embedding_file.write(
+                            json.dumps({'question': entry['question'], 'embedding': questions_embedding}) + '\n'
+                        )
+                        questions[entry['question']] = questions_embedding
+
+                    self.stdout.write(self.style.SUCCESS(f'question number {i} embedding saved successfully'))
+
+            file.seek(0)
             for i, line in enumerate(file):
                 entry = json.loads(line)
                 question = entry['question']
                 count += 1
 
-                if most_related_document(conversation.id, question) == entry['doc']:
+                if most_related_document(conversation.id, question, questions.get(question)) == entry['doc']:
                     correct_count += 1
                     self.stdout.write(self.style.SUCCESS(f'doc number {i} retrieval succeeded'))
                 else:
